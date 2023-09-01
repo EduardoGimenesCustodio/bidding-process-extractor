e07d6f7d34dccd7e0ab1743e3c1c313fe8906eaf
Eduardo Henrique Gimenes Custodio <eduardo.gimenes.custodio@gmail.com>
2023-09-01 12:15:41 -0400

    feat: init config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProcessesModule } from './processes/processes.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [ProcessesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    ProcessesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
