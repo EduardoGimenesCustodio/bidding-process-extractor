d3efa64d56cbac24d3723877f3380c902c5103cf
Eduardo Henrique Gimenes Custodio <eduardo.gimenes.custodio@gmail.com>
2023-09-02 11:18:12 -0400

    feat: process crud

--- src/processes/processes.controller.ts
+++ src/processes/processes.controller.ts
@@ -10,33 +10,37 @@ import {
 import { ProcessesService } from './processes.service';
 import { CreateProcessDto } from './dto/create-process.dto';
 import { UpdateProcessDto } from './dto/update-process.dto';
+import { IProcess } from './interfaces/process.interface';
 
 @Controller('processes')
 export class ProcessesController {
   constructor(private readonly processesService: ProcessesService) {}
 
   @Post()
-  create(@Body() createProcessDto: CreateProcessDto) {
+  create(@Body() createProcessDto: CreateProcessDto): Promise<void> {
     return this.processesService.create(createProcessDto);
   }
 
   @Get()
-  findAll() {
-    return this.processesService.findAll();
+  async findAll(): Promise<IProcess[]> {
+    return await this.processesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<IProcess> {
     return this.processesService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProcessDto: UpdateProcessDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateProcessDto: UpdateProcessDto,
+  ): Promise<void> {
     return this.processesService.update(+id, updateProcessDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: string): Promise<void> {
     return this.processesService.remove(+id);
   }
 }
--- src/processes/processes.service.ts
+++ src/processes/processes.service.ts
@@ -1,26 +1,103 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProcessDto } from './dto/create-process.dto';
 import { UpdateProcessDto } from './dto/update-process.dto';
+import { DataSource } from 'typeorm';
+import { Process } from './entities/process.entity';
+import { IProcess } from './interfaces/process.interface';
 
 @Injectable()
 export class ProcessesService {
-  create(createProcessDto: CreateProcessDto) {
-    return 'This action adds a new process';
+  constructor(private dataSource: DataSource) {}
+
+  async create(createProcessDto: CreateProcessDto): Promise<void> {
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const process: Process = createProcessDto;
+      await queryRunner.manager.save(process);
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
   }
 
-  findAll() {
-    return `This action returns all processes`;
+  async findAll(): Promise<IProcess[]> {
+    let processes: IProcess[];
+
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      processes = await queryRunner.manager.find(Process);
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+
+      return processes;
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} process`;
+  async findOne(id: number): Promise<IProcess> {
+    let process: IProcess;
+
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      process = await queryRunner.manager.findOne(Process, { where: { id } });
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+
+      return process;
+    }
   }
 
-  update(id: number, updateProcessDto: UpdateProcessDto) {
-    return `This action updates a #${id} process`;
+  async update(id: number, updateProcessDto: UpdateProcessDto): Promise<void> {
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      await queryRunner.manager.update(Process, id, updateProcessDto);
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} process`;
+  async remove(id: number): Promise<void> {
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const process = await queryRunner.manager.findOne(Process, {
+        where: { id },
+      });
+      await queryRunner.manager.remove(process);
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
