aa4e2410c6a5fe3d2d9aa35440f8739f6c284d66
Eduardo Henrique Gimenes Custodio <eduardo.gimenes.custodio@gmail.com>
2023-10-13 19:13:36 -0400

    fix: remove listener

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -3,13 +3,12 @@ import { ItemsService } from './items.service';
 import { ItemsController } from './items.controller';
 import { ItemModel } from '../infra/database/typeOrm/models/item.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ExtractItemsListener } from './listeners/extract-items.listener';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ItemModel]), HttpModule],
   exports: [TypeOrmModule],
   controllers: [ItemsController],
-  providers: [ItemsService, ExtractItemsListener],
+  providers: [ItemsService],
 })
 export class ItemsModule {}
